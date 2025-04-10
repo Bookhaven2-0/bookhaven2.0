a7a678d20de63cfac698022be313edbf123701b0
Thomas Hawley <thawley22@turing.edu>
2025-04-10 12:36:56 -0600

    Add validations to Book, setup enum for LibraryBook

---
+++ app/controllers/libraries/library_books_controller.rb
@@ -0,0 +1,9 @@
+class Libraries::LibraryBooksController < ApplicationController
+  
+  def create
+  end
+
+  def destroy 
+
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
 
   has_many :library_books 
   has_many :libraries, through: :library_books
+
+  validates :title, presence: true
+  validates :author, presence: true 
+  validates :description, presence: true
+
 end
