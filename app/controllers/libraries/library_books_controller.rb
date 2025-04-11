c914e69b851e8bcf6604966c149af14b8d6f6607
Thomas Hawley <thawley22@turing.edu>
2025-04-11 17:38:32 -0600

    WIP library_books#create

--- app/controllers/libraries/library_books_controller.rb
+++ app/controllers/libraries/library_books_controller.rb
@@ -6,10 +6,25 @@ class Libraries::LibraryBooksController < ApplicationController
   end
 
   def create
+    library = Library.find(params[:library_id])
+    isbn = :isbn_10 if book_params[:isbn].strip.size == 10
+    isbn = :isbn_13 if book_params[:isbn].strip.size == 13
+
+    book = Book.find_by(isbn => book_params[:isbn])
+
+    if book 
+      LibraryBook.create(library: library, book: book)
+      redirect_to library, notice: "#{book.title} has been added."
+    end
   end
 
   def destroy 
 
   end
+
+  private 
+    def book_params 
+      params.require(:book).permit(:isbn, :title, :author)
+    end
 
 end
