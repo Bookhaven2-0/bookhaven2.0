bbfb239a7a7d6b72062b5e1b70239d939276b2b0
Thomas Hawley <thawley22@turing.edu>
2025-04-13 16:55:48 -0600

    Change devise redirect after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :authenticate_user!
   # allow_browser versions: :modern
+  def after_sign_in_path_for(resources)
+    libraries_path
+  end
 end
